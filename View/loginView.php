4bebb9261643ee8b4d11028942ac09e2c6f7728b
Johnny Keniti Mukai <125989975+KenZohn@users.noreply.github.com>
2025-03-28 10:04:44 -0300

    Tela de login e cadastro criado

---
+++ View/loginView.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Gastrite</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <form class="pure-form pure-form-aligned"  method="POST" action="../controller/index.php">
+            <h1>Gastritos</h1>
+            <h2>Login</h2>
+            <fieldset>
+                <div class="pure-control-group">
+                    <input id="aligned-name" type="text" placeholder="E-mail" name="cx_usuario"/>
+                </div>
+                <div class="pure-control-group">
+                    <input id="aligned-password" type="password" placeholder="Senha" name="cx_senha"/>
+                </div>
+                <div class="pure-control">
+                    <button type="submit" class="pure-button pure-button-primary" name="bt1">Entrar</button>
+                </div>
+                <div class="pure-control">
+                    <button type="button" onclick="window.location.href='../View/cadastroView.php';" class="pure-button" id="btCadastrar" name="bt2">Criar novo usuário</button>
+                </div>
+            </fieldset>
+        </form>
+
+        <?php
+        if (isset($_GET['error'])) {
+            echo "Login falhou. Tente novamente.";
+        }
+        ?>
+    </body>
+</html>
