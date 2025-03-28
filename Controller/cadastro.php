a58a6b46667c8b3bf2661d9b49dbf4bbcdf7ee3b
João Guilherme <joaorato58@gmail.com>
2025-03-28 19:57:59 -0300

    fix: corrige funcionamento dos controllers "cadastro.php" e "login.php"

--- Controller/cadastro.php
+++ Controller/cadastro.php
@@ -1,13 +1,13 @@
 <?php
     session_start();
-    require_once '../model/CadastroUsuario.php';
+    require_once '../model/usuarioModel.php';
 
     if ($_SERVER['REQUEST_METHOD']=='POST'){
-        $usuario = $_POST['cx_usuario'];
+        $usuario = $_POST['cx_nome'];
         $senha = $_POST['cx_senha'];
         $email = $_POST['cx_email'];
 
-        $userModel = new CadastroUsuario();
+        $userModel = new Usuario();
         $resultado = $userModel -> cadastrarUsuario($usuario, $senha, $email);
 
         if ($resultado) {
