fb859ce3157f8242e3c17aa59b6bcb9c0332065a
João Crespin <164439947+joaocrespin@users.noreply.github.com>
2025-03-28 11:20:44 -0300

    update: controller "cadastro.php" modificado para atender o novo modelo

--- Controller/cadastro.php
+++ Controller/cadastro.php
@@ -1,13 +1,14 @@
 <?php
     session_start();
-    require_once '../model/UsuarioModel.php';
+    require_once '../model/CadastroUsuario.php';
 
     if ($_SERVER['REQUEST_METHOD']=='POST'){
         $usuario = $_POST['cx_usuario'];
         $senha = $_POST['cx_senha'];
         $email = $_POST['cx_email'];
 
-        $resultado = $userModel->cadastrarUsuario($usuario, $senha, $email);
+        $userModel = new CadastroUsuario();
+        $resultado = $userModel -> cadastrarUsuario($usuario, $senha, $email);
 
         if ($resultado) {
             $_SESSION['user'] = $usuario;
